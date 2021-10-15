4a7e541bf0ab7cb5589a58d454dba69ace4d06cb
jnsaksham <40722803+jnsaksham@users.noreply.github.com>
2021-10-14 23:46:48 -0400

    Update ass2_functions.py

--- ass2_functions.py
+++ ass2_functions.py
@@ -143,3 +143,50 @@ def extract_spectral_flux(xb):
     spectral_flux = np.array(spectral_flux)
 
     return spectral_flux
+
+def extract_spectral_centroid(xb, fs):
+    xb_afterFFT = np.zeros((np.shape(xb)[0], np.shape(xb)[1]))
+    for flag, i in enumerate(xb):
+        iw = i * np.hanning(np.shape(xb)[1])
+        iw = scipy.fftpack.fft(iw)
+        xb_afterFFT[flag] = iw
+    xb_afterFFT = np.absolute(xb_afterFFT)
+    vsc = np.zeros(np.shape(xb_afterFFT)[0])
+    half_k = np.shape(xb_afterFFT)[1]/2
+    for flag_centroid, block in enumerate(xb_afterFFT):
+        left = math.log(np.arange(np.shape(xb_afterFFT)[1]/2)*flag_centroid/fs, 2)
+        vsc[flag_centroid] = np.sum(left * xb_afterFFT[flag_centroid, 0:half_k]) / np.sum(xb_afterFFT[flag_centroid, 0:half_k])
+    return vsc
+
+
+def extract_zerocrossingrate(xb):
+    vzc = np.zeros(np.shape(xb)[0])
+    for n, block in enumerate(xb):
+        vzc[n] = 0.5 * np.mean(np.abs(np.diff(np.sign[block])))
+    return vzc
+
+## Wrapper functions ##
+
+def extract_features(x, blockSize, hopSize, fs):
+    block, timeInSec = block_audio(x, blockSize, hopSize, fs)
+    Vsc = extract_spectral_centroid(block, fs)
+    Vzc = extract_zerocrossingrate(block)
+    features = np.vstack(Vsc, Vzc)
+    return features
+
+
+def aggregate_feature_per_file(features):
+    feature_matrix = np.zeros(10)
+    for n, element in enumerate(features):
+        feature_matrix[n] = np.mean(element)
+        feature_matrix[n+5] = np.std(element)
+    return feature_matrix
+
+
+def get_feature_data(path, blockSize, hopSize):
+    all_feature_matrix = np.zeros((0, 10))
+    folder = glob.glob(path + "\\*.wav")
+    for count, wavFile in enumerate(folder):
+        fs, x = scipy.io.wavfile.read(wavFile)
+        all_feature_matrix = np.vstack((all_feature_matrix, aggregate_feature_per_file(extract_features(x, blockSize, hopSize, fs))))
+    return all_feature_matrix
